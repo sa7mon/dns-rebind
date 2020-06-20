fb9b6a763ebf1c2ec80b546067063dfc6363d6f3
Dan Salmon <dan.sa7mon@gmail.com>
2020-06-19 20:38:58 -0500

    Add default handler to log requests

--- password-server/main.go
+++ password-server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
@@ -17,6 +18,7 @@ func main() {
 	var strPort = strconv.Itoa(port)
 
 	r := mux.NewRouter()
+	r.NotFoundHandler = http.HandlerFunc(CustomNotFoundHandler)
 	r.HandleFunc("/getPasswords", GetPasswordsHandler)
 
 	srv := &http.Server{
@@ -33,4 +35,10 @@ func GetPasswordsHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("Serving request from: " + r.RemoteAddr)
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("admin:mysupercrazypa$$w0rd\n"))
+}
+
+func CustomNotFoundHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println(fmt.Sprintf("%s %s %s", r.RemoteAddr, r.Method, r.RequestURI))
+	w.WriteHeader(http.StatusNotFound)
+	w.Write([]byte("404 page not found"))
 }
